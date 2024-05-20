dc5f246404d5df19227f16f037e909713d15d8cc
Keerthika-1003 <159152221+Keerthika-1003@users.noreply.github.com>
2024-05-20 16:36:13 +0530

    Add files via upload

---
+++ FinancialAccountStateChangeEvent.java
@@ -0,0 +1,27 @@
+//package com.example.tmf666.Entity;
+//
+//import jakarta.persistence.Entity;
+//import jakarta.persistence.Id;
+//import lombok.Data;
+//import lombok.Getter;
+//import lombok.Setter;
+//
+//import java.time.LocalDateTime;
+//
+//@Setter
+//@Getter
+//@Data
+//public class FinancialAccountStateChangeEvent {
+//
+//    private String href;
+//    private String eventId;
+//    private LocalDateTime eventTime;
+//    private String eventType;
+//    private String correlationId;
+//    private String domain;
+//    private String title;
+//    private String description;
+//    private String priority;
+//    private LocalDateTime timeOccurred;
+//    private FinancialAccount payload;
+//}
---
+++ SettlementAccountStateChangeEventPayload.java
@@ -0,0 +1,28 @@
+//package com.example.tmf666.Entity;
+//
+//import jakarta.persistence.Entity;
+//import jakarta.persistence.Id;
+//import lombok.Data;
+//import lombok.Getter;
+//import lombok.Setter;
+//
+//import java.time.LocalDateTime;
+//@Entity
+//@Setter
+//@Getter
+//@Data
+//public class SettlementAccountStateChangeEventPayload {
+//    @Id
+//    private String id;
+//    private String href;
+//    private String eventId;
+//    private LocalDateTime eventTime;
+//    private String eventType;
+//    private String correlationId;
+//    private String domain;
+//    private String title;
+//    private String description;
+//    private String priority;
+//    private LocalDateTime timeOccurred;
+//    private SettlementAccount settlementAccount;
+//}
